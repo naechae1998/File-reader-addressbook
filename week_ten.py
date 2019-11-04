5cd0efeff6a1b488bac1fe737d814399e73bba3f
naechae1998 <57082635+naechae1998@users.noreply.github.com>
2019-11-03 23:18:08 -0600

    Add files via upload

---
+++ week_ten.py
@@ -0,0 +1,19 @@
+import os #Still not Quite sure what an OS library is but getting a good grip on using the import function
+filep =input("please enter directory to store file ")
+filePath = filep
+fN= input ("filename: ");
+completePath = filePath+fN
+if os.path.isfile(fN): # file exists?
+ print('File Exists')
+else:
+  open(fN, "w+")
+if os.path.isdir(filePath): # file path exists?
+ print('The directory is Valid')
+if os.path.exists(completePath): #complete path exist?
+ print(' It is a complete path!')
+print('Your Complete Path is: ',completePath)
+with open(fN, 'w+') as fileHandle: #open file for writing
+ input("please Enter address Name and Phone Number: ")
+print(fN)
+print (fN.readlines()) #this is supposed to show what is inside the file I keep getting errors like theAttributeError: 'str' object has no attribute
+
